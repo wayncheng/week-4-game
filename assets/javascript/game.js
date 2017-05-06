ffbbe65fdcaeebdd2e75f0e3ffb25fc1d7f4b316
wayne <wayne@che.ng>
2017-05-06 01:11:02 -0700

    1.0.2 - fully functional release

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,169 +1,223 @@
 $(document).ready(function(){
 
 	var character = [
-		{ name: 'c3po', 			hp: 140, 	attack: 7, 	counter: 4 },
-		{ name: 'r2-d2', 			hp: 120, 	attack: 7, 	counter: 5 },
-		{ name: 'yoda', 			hp: 205, 	attack: 12,	counter: 10},
-		{ name: 'bb-8', 			hp: 150, 	attack: 8, 	counter: 6 },
-		{ name: 'chewbacca', 	hp: 200, 	attack: 10, 	counter: 8 },
+		{ name: 'c3po', 			title: 'C-3PO',			hp: 140, 	attack: 7, 	counter: 4 },
+		{ name: 'r2-d2', 			title: 'R2-D2',			hp: 120, 	attack: 7, 	counter: 5 },
+		{ name: 'yoda', 			title: 'Yoda',			hp: 205, 	attack: 12,	counter: 10},
+		{ name: 'bb-8', 			title: 'BB-8',			hp: 150, 	attack: 8, 	counter: 6 },
+		{ name: 'chewbacca', 	title: 'Chewbacca',	hp: 200, 	attack: 10, 	counter: 8 },
 		
-		{ name: 'emperor', 			hp: 110, 	attack: 6, 	counter: 20},
-		{ name: 'stormtrooper', hp: 90,		attack: 5, 	counter: 10},
-		{ name: 'darth-vader', 	hp: 160,	attack: 10,	counter: 25},
-		{ name: 'darth-maul', 	hp: 120,	attack: 8, 	counter: 20},
-		{ name: 'boba-fett', 		hp: 110,	attack: 7, 	counter: 15},
+		{ name: 'emperor', 			title: 'The Emperor',		hp: 110, 	attack: 6, 	counter: 20},
+		{ name: 'stormtrooper', title: 'Stormtrooper',	hp: 90,		attack: 5, 	counter: 10},
+		{ name: 'darth-vader', 	title: 'Darth Vader',		hp: 160,	attack: 10,	counter: 25},
+		{ name: 'darth-maul', 	title: 'Darth Maul',		hp: 120,	attack: 8, 	counter: 20},
+		{ name: 'boba-fett', 		title: 'Boba Fett',			hp: 110,	attack: 7, 	counter: 15},
 	];
 
 	var user, enemy, u, e;
-	var uHP, uA, uAi, uCA, eHP, eCA;
+	var uHP, uHPi, uA, uAi, uCA, eHP, eHPi, eCA;
 	var uHPcur, uAcur;
-	var round = 1;
 
 	// indeces for selected characters in character array
 	var uI, eI;
 
 ////////////////////////////////////////////////////////////////////////////////
 	function restart() {
-		var original;
-	}
+		var eSel = '#'+ enemy;
+		var uSel = '#'+ user
 
-////////////////////////////////////////////////////////////////////////////////
-	function badSelect(){
+			$(uSel).show();
+			var uImg = $('#good-area').find('img');
+			$(uSel).append(uImg);
+
+			$(eSel).show();
+			var eImg = $('#bad-area').find('img');
+			$(eSel).append(eImg);
+
+			$('.hp-text').text('');
+
+			$('.hp-bar').css({
+					'width': '100%',
+					'background-color': '#2ecc71'
+				});
+
+			// Reset All Values
+			user = enemy = u = e = null;
+			uHP = uHPi = uA = uAi = uCA = eHP = eHPi = eCA = null;
+			eI = uI = null;
+
+			readyToAttack();
+
+			// clear status
+			$('.arena-area').attr('status','empty');
+
+			$('img.good').on('click',userSelect);
+			$('img.good').removeClass('inactive');
+
+			// Remove handler from attack button
+			$('#attack').off('click',attack);
+		
+		}
+	$('#restart').on('click',restart);
+
+
+function badSelect(){
 		var $t = $(this);
 		var $p = $(this).parent();
 		enemy = $p.attr('id');
-		var $area = $('#defender-area');
+		var $area = $('#bad-area');
 		var s = $area.attr('status');
-		console.log($t); 
-		console.log('enemy',enemy); 
+
+		// Find Enemy Index
+		for (var i=0; i<character.length; i++) {
+			if ( character[i].name === enemy )
+				eI = i;
+		};
+		e = character[eI];
+		eHP = e['hp'];
+		eHPi = e['hp'];
+		eCA = e['counter'];
 
 		if (s === 'empty') {	
 			// hide image wrap in row of chars
-			$t.parent().css('display','none');
+			$t.parent().hide();
 			$area.append($t);
 			$area.attr('status','taken');
+			$('#bad-area .hp-text').text(eHP);
 		}
+
+		$('.hp-bar.bad').css('width','100%');
+		$('.hp-bar.bad').css('background-color','#2ecc71');
 
 		return enemy;
 	}
 
-
-////////////////////////////////////////////////////////////////////////////////
-	function userSelect(){
+function userSelect(){
 		var $t = $(this);
 		user = $t.parent().attr('id');
 		var $area = $('#good-area');
 		var s = $area.attr('status');
 
+		// Find User Index
+		for (var i=0; i<character.length; i++) {
+			if ( character[i].name === user )
+				uI = i;
+		};
+
+		// Prep User Values
+		u = character[uI];
+		uHP = u['hp'];
+		uHPi = u['hp'];
+		uA = u['attack'];
+		uAi = u['attack'];
+
+		// Only selects character if nothing else is selected.
 		if ( s === 'empty') {	
 			// hide image wrap in row of chars
-			$t.parent().css('display','none');
+			$t.parent().hide();
 			$area.append($t);
 			$area.attr('status','taken');
+			$('#good-area .hp-text').text(uHP);
 		}
-		console.log('user',user);
+
+		// Remove click handler from user selection
+		$('img.good').off('click',userSelect);
+		$('img.good').addClass('inactive');
+
 		return user;
 	}
 
-	$('img.bad').on('click',badSelect);
-	$('img.good').on('click',userSelect);
-
-
-////////////////////////////////////////////////////////////////////////////////
-	function nextEnemy(){
+function nextEnemy(){
 		// Remove enemy img form arena area
-		$('#defender-area img').css('display','none');
+		$('#bad-area img').css('display','none');
 
 		// Change background color of attack button
-		$('#attack').css('background-color','#7f8c8d');
+		$('#attack').removeClass('inactive');
+
 		// Remove handler from attack button, which deactivates it.
 		$('#attack').off('click',attack);
 
 		// Reset enemy value to null;
 		enemy = null;
 
-		// Reset occupancy status of defender area
-		$('#defender-area').attr('status','empty');
+		// Reset occupancy status of bad area
+		$('#bad-area').attr('status','empty');
 	}
 
-////////////////////////////////////////////////////////////////////////////////		
-	function attack(){
-		// Increment user attack up, initially set at 0, so must increment before attack
-		// uA += u_A;
-
+function attack(){
 		// User Attacks
 			// current enemy HP minus attack value
 			eHP -= uA;
-			console.log('uA for',uA,'eHP now',eHP); 
 
 		// Enemy Counters
 			uHP -= eCA;
-			console.log('eCA for',eCA,'uHP now', uHP);
+
+		// Update HP bars, values
+			$('.hp-text.good').text(uHP);
+			$('.hp-text.bad').text(eHP);
+
+			var uW = (uHP/uHPi)*100;
+			var eW = (eHP/eHPi)*100;
+
+			$('.hp-bar.good').animate({'width': uW+'%'}, 300);
+			$('.hp-bar.bad').animate({'width': eW+'%'}, 300);
+
+		// Change HP bar color when low
+			if (uW <= 30) 			$('.hp-bar.good').css('background-color','#e74c3c');
+			else if (uW <= 60) 	$('.hp-bar.good').css('background-color','#f39c12');
+			else							 	$('.hp-bar.good').css('background-color','#2ecc71');
+			
+			if (eW <= 30) 			$('.hp-bar.bad').css('background-color','#e74c3c');
+			else if (eW <= 60) 	$('.hp-bar.bad').css('background-color','#f1c40f');
+			else 								$('.hp-bar.bad').css('background-color','#2ecc71');
+
+		// Feed Text
+			var uT = u['title'];
+			var eT = e['title'];
+			var feed1text = uT + ' attacked ' + eT + ' for ' + uA + ' HP. ' + eT + ' now has ' + eHP + ' HP left.';	
+			var feed2text = eT + ' attacked ' + uT + ' for ' + eCA + ' HP. ' + uT + ' now has ' + uHP + ' HP left.';
+
+		// Check for Deaths
+			if ( eHP <= 0 ) {
+				feed1text = uT + ' attacked ' + eT + ' for ' + uA + 'HP. ' + eT + ' has died. Select your next opponent.';
+				nextEnemy ();
+			}
+			if ( uHP <= 0 ) {
+				feed2text = eT + ' attacked ' + uT + ' for ' + eCA + 'HP. ' + uT + 'has died. Game Over.';
+				console.log('---------GAME OVER-----------');
+			}
+
+		// Update Feeds
+			$('#feed1').text(feed1text);
+			$('#feed2').text(feed2text); 
+			
+				console.log(feed2text);
+				console.log(feed1text);
 
 		// Increment User's attack value by initial value of attack
 			uA += uAi;
 			console.log('new uA', uA);
-
-		// Check for Deaths
-		if ( eHP <= 0 ) {
-			nextEnemy ();
-		}
-		if ( uHP <= 0 ) {
-			console.log('---------GAME OVER-----------');
-		}
-
-
 	} 
 
-
-////////////////////////////////////////////////////////////////////////////////
-	function readyToAttack(){
+function readyToAttack(){
 		// When both characters selected, their variables will be strings, 
 		// and we can activate the attack button
 		if ( $.type(user) === 'string' && $.type(enemy) === 'string' ){
+					// Change background color
+					$('#attack').removeClass('inactive');
 
-			for (var i=0; i<character.length; i++) {
-				var ix = character[i].name;
-				if ( ix === user ) {
-					uI = i;
-				}
-				else if ( ix === enemy ){
-					eI = i;
-				}
-			};
+					// Attach handler to attack button, which activates it.
+					$('#attack').on('click',attack);
 
-
-			// Prep Enemy Values
-			e = character[eI];
-			eHP = e['hp'];
-			eCA = e['counter'];
-
-			if ( round <= 1 ) {
-				// Prep User Values
-				u = character[uI];
-				uHP = u['hp'];
-				uA = u['attack'];
-				uAi = u['attack'];
-			}
-
-
-
-			// Change background color
-			$('#attack').css('background-color','#3498db');
-
-			// Attach handler to attack button, which activates it.
-			$('#attack').on('click',attack);
-
-			// Increment round
-			round++;
-
-		} //////////// end if() ////////////
-
+					$('img.good').off('click',userSelect);
+		}
 	} // end readytoAttack()
 
 
-	$('img.char').on('click',readyToAttack);
 
+$('img.bad').on('click',badSelect);
+$('img.good').on('click',userSelect);
+$('img.char').on('click',readyToAttack);
 
 
 }) ////////////////////////////////////////////////////////////
